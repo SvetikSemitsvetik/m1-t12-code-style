e4fbeeda33d8a1fa11ca0ce33c60f2019962179b
Kirill Sudakov <kirill.sudakov.01@mail.ru>
2023-04-23 20:43:39 +0300

    make refactoring

---
+++ src/CalculateDeposit.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class CalculateDeposit {
+    double calculateComplexPercent(double a, double y, int d) {
+        double pay = a * Math.pow((1 + y / 12), 12 * d);
+
+        return round(pay, 2);
+    }
+
+    double calculateSimplePercent(double doubleAmount, double doubleYearRate, int depositPeriod) {
+        return round(doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
+    }
+
+    double round(double value, int places) {
+        double scale = Math.pow(10, places);
+
+        return Math.round(value * scale) / scale;
+    }
+
+    void doImportantJob() {
+        int period;
+        int action;
+        int amount;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях: ");
+        amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        double contribution = 0;
+
+        if (action == 1)  {
+            contribution = calculateSimplePercent(amount, 0.06, period);
+        } else if (action == 2) {
+            contribution = calculateComplexPercent(amount, 0.06, period);
+        }
+
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + contribution);
+    }
+
+    public static void main(String[] args) {
+        new CalculateDeposit().doImportantJob();
+    }
+}
